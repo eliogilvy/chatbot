90cba46abd8d123dca227c37870b8f1b7330c690
Eli Ogilvy <eogilvy@seattleu.edu>
2023-05-23 22:46:52 -0700

    alignment update

--- lib/widgets/home/welcome.dart
+++ lib/widgets/home/welcome.dart
@@ -11,36 +11,39 @@ class WelcomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final auth = Provider.of<AuthProvider>(context);
-    return FutureBuilder<String>(
-      future: auth.getUserName(),
-      builder: (context, snapshot) {
-        return snapshot.connectionState == ConnectionState.waiting
-            ? const FallingDot()
-            : snapshot.data != null && snapshot.data!.isNotEmpty
-                ? Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      padding: const EdgeInsets.all(4),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Theme.of(context).colorScheme.primary,
+    return Align(
+      alignment: Alignment.topLeft,
+      child: FutureBuilder<String>(
+        future: auth.getUserName(),
+        builder: (context, snapshot) {
+          return snapshot.connectionState == ConnectionState.waiting
+              ? const FallingDot()
+              : snapshot.data != null && snapshot.data!.isNotEmpty
+                  ? Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        child: AnimatedTextKit(
+                          animatedTexts: [
+                            TyperAnimatedText(
+                              'Welcome, ${snapshot.data}',
+                              speed: const Duration(milliseconds: 75),
+                              textStyle:
+                                  Theme.of(context).textTheme.headlineMedium,
+                              curve: Curves.slowMiddle,
+                            ),
+                          ],
+                          isRepeatingAnimation: false,
+                        ),
                       ),
-                      child: AnimatedTextKit(
-                        animatedTexts: [
-                          TyperAnimatedText(
-                            'Welcome, ${snapshot.data}',
-                            speed: const Duration(milliseconds: 75),
-                            textStyle:
-                                Theme.of(context).textTheme.headlineMedium,
-                            curve: Curves.slowMiddle,
-                          ),
-                        ],
-                        isRepeatingAnimation: false,
-                      ),
-                    ),
-                  )
-                : const Text('Unknown Error');
-      },
+                    )
+                  : const Text('Unknown Error');
+        },
+      ),
     );
   }
 }
